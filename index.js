04a3014d5c24b79dc0985af041688c112dfc620d
mindong2 <qlsnaos12@naver.com>
2023-12-15 23:26:57 +0900

    dfs

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-// 간단한 그리디 예제
+// cur은 시작지점
+function dfs(graph, visit, cur) {
+  if (visit[cur]) return;
+  visit[cur] = 1;
+  console.log(cur);
 
-let K = 10;
-let M = 4790;
+  graph[cur].forEach((next) => {
+    dfs(graph, visit, next);
+  });
+}
 
-let arr = [1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000];
+// 정점쌍(u, v)의 수
+const N = 12;
 
-let answer = 0;
-arr.reverse();
+// 정점의 수
+const M = 13;
 
-arr.forEach((money) => {
-  while (M - money >= 0) {
-    M -= money;
-    answer++;
-    if (M === 0) return;
-  }
-});
+let graph = Array.from({ length: M + 1 }, () => []);
+let visit = Array.from({ length: M + 1 }, () => 0);
 
-console.log(answer);
+// 그래프 생성
+graph[1].push(2);
+graph[1].push(3);
+graph[1].push(4);
+graph[3].push(5);
+graph[3].push(6);
+graph[5].push(7);
+graph[5].push(8);
+graph[6].push(9);
+graph[9].push(10);
+graph[9].push(11);
+graph[10].push(12);
+graph[10].push(13);
+
+dfs(graph, visit, 1);
